cb22867f3472f72f1f7f8a8aa3789e02274e07cb
julidelcris <julidelcris@gmail.com>
2024-01-14 01:02:22 +0000

    update: main.py and gitignore

--- src/main.py
+++ src/main.py
@@ -27,24 +27,10 @@ def command_flutter(common, core, data, domain, features):
     CreateFolder.command_flutter(common=common, core=core, data=data, domain=domain, features=features)     
 
 @cli.command()
-@click.option('--features', '-f', is_flag=True, help='Crear carpeta y estructura dentro de "features".')
-def flutter_features(features):
-    """Crea carpetas con código por defecto para proyectos Flutter en la carpeta "features"."""
-    if features:
-        try:
-            carpeta_features = CarpetaHelper.buscar_carpeta_features()
-        except click.ClickException as e:
-            click.echo(str(e))
-            return
-    else:
-        carpeta_features = click.prompt('Ingrese la ruta de la carpeta "features"', type=click.Path(file_okay=False, resolve_path=True))
-
-    nombre_carpeta = click.prompt('Nombre de la carpeta en "features"', type=str)
-    carpetas_flutter = ['features', nombre_carpeta, 'presentation', 'domain', 'data']
-    click.echo(f'Carpeta seleccionada: {nombre_carpeta}')
-
-    CarpetaHelper.crear(nombre_proyecto=carpeta_features, carpetas=carpetas_flutter, nombre_carpeta=nombre_carpeta)
-    Configuracion.guardar(carpeta_features)
+@click.option('--soon', '-s', is_flag=True, help='Crear carpeta y estructura dentro de "features".')
+def coming_soon(features):
+    """Crea carpetas con código por defecto para otros proyectos muy pronto..."""
+    click.echo('Proximamente...')
 
 if __name__ == '__main__':
     cli()
